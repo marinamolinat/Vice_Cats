4d604ee8159b6858df0e0d6853dc4fe24c1b3413
Marina M <marinamolina7877@gmail.com>
2025-07-13 15:36:37 -0500

    Made a workable popup that asks if the usr truly want to watch a video

--- Content.js
+++ Content.js
@@ -6,10 +6,17 @@ new MutationObserver(() => {
   if (currentUrl !== lastUrl) {
     lastUrl = currentUrl;
 
-    // Check if it's a video URL
+    // Check if its a video URL
     if (currentUrl.includes("watch?v=")) {
-      console.log("🎥 Navigated to a YouTube video:", currentUrl);
-      // Send message to background or whatever you need
+      console.log(" Navigated to a YouTube video:", currentUrl);
+      const confirmed = confirm("Are you sure you want to watch this video?");
+
+      if (confirmed) {
+      window.location.href = link.href;
+      } else {
+      window.location.href = "https://www.youtube.com/"; 
+      }
+    
     }
   }
 }).observe(document, { subtree: true, childList: true });
